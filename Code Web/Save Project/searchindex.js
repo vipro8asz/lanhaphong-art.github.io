38e6ba6886b0781b8acf9346c1decd2ae56d55d5
vipro8asz <vipro8asz@vipro8asz.vn>
2019-02-11 16:45:07 +0700

    Trang Vip + Code

---
+++ Code Web/Save Project/searchindex.js
@@ -0,0 +1,19 @@
+// search index for WYSIWYG Web Builder
+var database_length = 0;
+
+function SearchPage(url, title, keywords, description)
+{
+   this.url = url;
+   this.title = title;
+   this.keywords = keywords;
+   this.description = description;
+   return this;
+}
+
+function SearchDatabase()
+{
+   database_length = 0;
+   this[database_length++] = new SearchPage("Save_Project.html", "C Đình Phát, Nhã Phong TM", "Đình phát nhã phong tm headingsave project vipro8asz wbswysiwyg web builder download marquee chữ chạyvăn bản di chuyển panel menu gmail com page trang nbsp text area balloon bo suu tap anh blog hướng dẫn click view chọn guide settings bỏ lock để tự lấy kích thước chú Để search hàng muốn hiện thị màn hình sẽ tới không flash swf trong table lưu file html cùng tên với name wbs nó sai sau khi hoàn tất phần tử chuột phải select all cắt cut vào tạo khung kéo về đo dán paste và bảng xác định phù hợp ra khỏi height width sửa thành nhấn phím mũi lt publish read more send comments email gửi tin cho tác giả sunday december bài viết văn laquo prev next raquo like share ", "");
+   this[database_length++] = new SearchPage("Trang_1.html", "Untitled Page", "untitled page trang laquo prev next raquo ", "");
+   return this;
+}
