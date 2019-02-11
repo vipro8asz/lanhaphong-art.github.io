1b8ef19993fa80ba7f4aefc2c4a62ecfba402b50
vipro8asz <vipro8asz@vipro8asz.vn>
2019-02-11 16:52:52 +0700

    Đặc biệt

---
+++ Dac biet/Love Troll/Love_Troll.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function()
+{
+   $("a[data-rel='Slide_Show']").attr('rel', 'Slide_Show');
+   $('#Slide_Show img').attr('title', '');$('#Slide_Show').galleria({});
+});
+var kt=false
+alert('Hãy xoay ngang màn hình để hiện thị được tốt hơn')
+alert('Chào em ;)')
+alert('Web dành cho người mới quen')
+alert('Hi My Girl ~ I Super')
+alert('C Đình Phát, Nhã Phong TM')
+alert('Anh muốn hỏi <?')
+alert('Em muốn làm Bạn hay người yêu')
+alert('Hãy cho anh biết')
+alert('Đang chờ em duyệt')
+ 
+
+function move()
+{var a = Math.random()*80
+var b = Math.random()*80
+var x = Math.random()*80
+var y = Math.random()*80
+var left=a+'%'
+var right=b+'%'
+var top=y+'%'
+var bottom=x+'%'
+document.getElementById('Move').style.left=left
+document.getElementById('Move').style.right=right
+document.getElementById('Move').style.top=top
+document.getElementById('Move').style.bottom=bottom}
+function Thông_báo_Cick()
+{if(kt==false)
+{alert('Tình thì không thiếu chủ yếu chưa có người yêu')
+kt=true}
+else
+{alert('Yêu anh đi em anh không đòi quà')
+alert('>x<')
+alert('Nếu mà em hỏi: Anh có nguyện chết vì em không ?')
+alert('Thì câu trả lời là: Không còn sống lấy gì mà yêu ?')
+alert('...')
+alert('Chém gió vậy thôi chứ anh yêu em rất nhiều')
+alert('Chào em, Sea You Again, GoodBye, Hẹn gặp lại')
+alert(';)')}}
