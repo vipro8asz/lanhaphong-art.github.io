f35d2a2931a5f5ea222bc671490e66458fecefa6
Vipro8ASz <Vipro8ASz@Gmail.Com>
2019-03-06 14:09:33 +0700

    Update

---
+++ Dac_biet/Anh_yeu_em/jquery.global.js
@@ -0,0 +1,15 @@
+﻿
+jQuery(document).ready(function($) {
+
+	$('#cloud').pan({
+		fps: 60, // Frame trên giây, fps càng nhỏ thì chuyển động sẽ càng giật.
+		speed: 0.3, // tốc độ di chuyển của vậy thể
+		dir: 'left' // hướng chuyển động, left: phải>trái, right: trái>phải
+	});
+	$('#city').pan({
+		fps: 60, // Frame trên giây, fps càng nhỏ thì chuyển động sẽ càng giật.
+		speed: 1, // tốc độ di chuyển của vậy thể
+		dir: 'right' // hướng chuyển động, left: phải>trái, right: trái>phải
+	});
+
+});
